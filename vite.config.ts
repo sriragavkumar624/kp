78360f139d0e5eb61c1b2175b872bba96948eda1
sriragavkumar <sriragavkumar624@gmail.com>
2025-11-21 17:39:32 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: "/ragav/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/resume/",
+ 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
